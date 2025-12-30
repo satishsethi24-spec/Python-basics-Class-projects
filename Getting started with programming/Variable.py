2e992d6af92d54d1311b267b9b2e9a88e5a8103a
satishsethi24-spec <satishsethi24@gmail.com>
2025-12-30 20:01:22 +0530

    Add files via upload

---
+++ Getting started with programming/Variable.py
@@ -0,0 +1,6 @@
+print("hello everyone, I am learining coding it is nice",end="*")
+print( 99+50)
+Coding_teacher="Joyce" 
+Weight= 30.2
+print(Coding_teacher,"is my coding teacher")
+print("My weight is",Weight)
