4d93f735b80a587ef6767f08816ffd16b0ba3eea
satishsethi24-spec <satishsethi24@gmail.com>
2026-03-31 18:50:32 +0530

    Add files via upload

---
+++ Loops/Reverse_string.py
@@ -0,0 +1,5 @@
+str1= str(input("Enter the string"))
+str2= ""
+for i in str1:
+    str2=i+str2
+print(str2)
---
+++ Loops/Sum_of_numbers.py
@@ -0,0 +1,5 @@
+n= int(input("Enter a whole number"))
+sum= 0
+for i in range(1,n+1):
+    sum=sum+i
+print("The sum of your whole number is:", sum)
